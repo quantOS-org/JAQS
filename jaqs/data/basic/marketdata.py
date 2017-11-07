c39deee5d30f6564945f1fc5c21d952f7ecd1f88
brillliantz <brillliantz@outlook.com>
2017-11-07 10:01:01 +0800

    add trade_date, time as members; add __repr__, update create_from_df function

--- jaqs/data/basic/marketdata.py
+++ jaqs/data/basic/marketdata.py
@@ -10,14 +10,27 @@ class Bar(object):
         self.volume = 0
         self.oi = 0
         self.vwap = 0.
+        
+        self.trade_date = 0
+        self.time = 0
+        self._bar_keys = ['open', 'close', 'high', 'low', 'vwap',
+                          'volume', 'oi',
+                          'trade_date', 'time']
     
     @classmethod
     def create_from_df(cls, df):
-        dic_of_dic = df.to_dict(orient='index')
-        
+        row_dic_list = [row.to_dict() for idx, row in df.iterrows()]
         bar_list = []
-        for v in dic_of_dic.values():
+        for row_dic in row_dic_list:
             bar = cls()
-            bar.__dict__.update(v)
+            bar.__dict__.update(row_dic)
             bar_list.append(bar)
         return bar_list
+    
+    def __repr__(self):
+        return "{0.trade_date:8d}-{0.time:8d} " \
+               "{0.volume:5f} of " \
+               "{0.symbol:10s}@{0.close:.3f}".format(self)
+
+    def __str__(self):
+        return self.__repr__()
