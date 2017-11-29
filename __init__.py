7ec7fd88a84961640827c3fb433e3a3def141a06
brillliantz <brillliantz@outlook.com>
2017-11-29 11:25:42 +0800

    [PY3] re-format code

--- __init__.py
+++ __init__.py
@@ -2,15 +2,11 @@
 """
 Core data api for fetching data from remote service.
 """
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
 from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
-from future import standard_library
-standard_library.install_aliases()
-from builtins import *
 from .data_api import DataApi
-
 
 __all__ = ['DataApi']
