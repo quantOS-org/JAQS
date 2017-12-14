941bc629d6096fdcded27f5fc6cae06e757a795e
brillliantz <brillliantz@outlook.com>
2017-12-14 14:46:42 +0800

    [EG] simpler example

--- example/research/signal_return_ic_analysis.py
+++ example/research/signal_return_ic_analysis.py
@@ -9,7 +9,9 @@ from jaqs.data import RemoteDataService
 from jaqs.research import SignalDigger
 import jaqs.util as jutil
 
+# from config_path.py, we import the path of config files
 from config_path import DATA_CONFIG_PATH
+# we use read_json to read the config file to a dictionary
 data_config = jutil.read_json(DATA_CONFIG_PATH)
 
 dataview_folder = '../../output/prepared/test_signal'
@@ -26,42 +28,33 @@ def save_dataview():
     
     dv.init_from_config(props, ds)
     dv.prepare_data()
+
+    trade_status = dv.get_ts('trade_status')
+    mask_sus = trade_status == '停牌'
+    dv.append_df(mask_sus, 'suspended', is_quarterly=False)
+
+    dv.add_formula('not_index_member', '!index_member', is_quarterly=False)
+
+    dv.add_formula('limit_reached', 'Abs((open - Delay(close, 1)) / Delay(close, 1)) > 0.095', is_quarterly=False)
     
     dv.save_dataview(dataview_folder)
 
 
-def analyze_signal():
-    # --------------------------------------------------------------------------------
-    # Step.1 load dataview
-    dv = DataView()
-    dv.load_dataview(dataview_folder)
-
-    # --------------------------------------------------------------------------------
+def analyze_signal(dv, signal_name, output_format='pdf'):
     # Step.2 calculate mask (to mask those ill data points)
-    trade_status = dv.get_ts('trade_status')
-    mask_sus = trade_status == '停牌'
-
-    df_index_member = dv.get_ts('index_member')
-    mask_index_member = ~(df_index_member > 0)
-
-    dv.add_formula('limit_reached', 'Abs((open - Delay(close, 1)) / Delay(close, 1)) > 0.095', is_quarterly=False)
-    df_limit_reached = dv.get_ts('limit_reached')
-    mask_limit_reached = df_limit_reached > 0
-
+    mask_sus = dv.get_ts('suspended')
+    mask_index_member = dv.get_ts('not_index_member')
+    mask_limit_reached = dv.get_ts('limit_reached')
     mask_all = np.logical_or(mask_sus, np.logical_or(mask_index_member, mask_limit_reached))
 
-    # --------------------------------------------------------------------------------
-    # Step.3 get signal, benchmark and price data
-    dv.add_formula('divert', '- Correlation(vwap_adj, volume, 10)', is_quarterly=False)
-    
-    signal = dv.get_ts('divert').shift(1, axis=0)  # avoid look-ahead bias
+    signal = dv.get_ts(signal_name).shift(1, axis=0)  # avoid look-ahead bias
     price = dv.get_ts('close_adj')
     price_bench = dv.data_benchmark
 
     # Step.4 analyze!
     my_period = 5
     obj = SignalDigger(output_folder='../../output/test_signal',
-                       output_format='pdf')
+                       output_format=output_format)
     obj.process_signal_before_analysis(signal, price=price,
                                        mask=mask_all,
                                        n_quantiles=5, period=my_period,
@@ -70,6 +63,16 @@ def analyze_signal():
     res = obj.create_full_report()
     # print(res)
 
+
+def simple_test_signal():
+    dv = DataView()
+    dv.load_dataview(dataview_folder)
+    
+    dv.add_formula('open_jump', 'open_adj / Delay(close_adj, 1)', is_quarterly=False) # good
+    analyze_signal(dv, 'open_jump', 'pdf')
+    
+    print("Signal return & IC test finished.")
+
 if __name__ == "__main__":
-    save_dataview()
-    analyze_signal()
+    #save_dataview()
+    simple_test_signal()
