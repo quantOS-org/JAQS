cf9428b4a5a1f251d25fe0c1b004edf631cb789f
brillliantz <brillliantz@outlook.com>
2018-01-12 14:58:53 +0800

    [ENH] expose EventDataView

--- jaqs/data/__init__.py
+++ jaqs/data/__init__.py
@@ -7,9 +7,9 @@ Modules relevant to data.
 
 from .dataapi import DataApi
 from .dataservice import RemoteDataService, DataService
-from .dataview import DataView
+from .dataview import DataView, EventDataView
 from .py_expression_eval import Parser
 
 
 # we do not expose align and basic
-__all__ = ['DataApi', 'DataService', 'RemoteDataService', 'DataView', 'Parser']
+__all__ = ['DataApi', 'DataService', 'RemoteDataService', 'DataView', 'Parser', 'EventDataView']
