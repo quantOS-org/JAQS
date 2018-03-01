3a71886ac5a957dfdcead7077bf9f530c33979c4
manu34414 <lunli1226@gmail.com>
2018-03-01 17:00:10 +0800

    Add get_symbol() to DataView
    Add get_symbol() to DataView

--- jaqs/data/dataview.py
+++ jaqs/data/dataview.py
@@ -1144,7 +1144,17 @@ class DataView(object):
         df_ann.columns = df_ann.columns.droplevel(level='field')
     
         return df_ann
-    
+
+    def get_symbol(self, symbol, start_date=0, end_date=0, fields=""):
+        res = self.get(symbol, start_date=start_date, end_date=end_date, fields=fields)
+        if res is None:
+            raise ValueError("No data. for "
+                             "start_date={}, end_date={}, field={}, symbol={}".format(start_date, end_date,
+                                                                                      fields, symbol))
+
+        res.columns = res.columns.droplevel(level='symbol')
+        return res
+
     def get_ts_quarter(self, field, symbol="", start_date=0, end_date=0):
         # TODO
         sep = ','
@@ -2253,7 +2263,7 @@ class EventDataView(object):
         if res is None:
             raise ValueError("No data. for "
                              "start_date={}, end_date={}, field={}, symbol={}".format(start_date, end_date,
-                                                                                      field, symbol))
+                                                                                      fields, symbol))
 
         res.columns = res.columns.droplevel(level='symbol')
         return res
