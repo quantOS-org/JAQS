8e2b218d79610bde136b8e087eaae3d646a2c486
manu34414 <lunli1226@gmail.com>
2018-06-27 14:21:05 +0800

    Change the calculation method of quantilize_without_nan()
    Change the calculation method of quantilize_without_nan()

--- jaqs/util/numeric.py
+++ jaqs/util/numeric.py
@@ -6,17 +6,19 @@ import pandas as pd
 def quantilize_without_nan(mat, n_quantiles=5, axis=-1):
     # mask = np.isnan(mat)
     mask = pd.isnull(mat)
+    res = mat.copy()
+    res[~mask] = pd.qcut(mat[~mask], n_quantiles, labels = False) + 1
 
-    rank = mat.argsort(axis=axis).argsort(axis=axis)  # int
-    
-    count = np.sum(~mask, axis=axis)  # int
-    divisor = count * 1. / n_quantiles  # float
-    shape = list(mat.shape)
-    shape[axis] = 1
-    divisor = divisor.reshape(*shape)
-    
-    res = np.floor(rank / divisor) + 1.0
-    res[mask] = np.nan
+    # rank = mat.argsort(axis=axis).argsort(axis=axis)  # int
+    #
+    # count = np.sum(~mask, axis=axis)  # int
+    # divisor = count * 1. / n_quantiles  # float
+    # shape = list(mat.shape)
+    # shape[axis] = 1
+    # divisor = divisor.reshape(*shape)
+    #
+    # res = np.floor(rank / divisor) + 1.0
+    # res[mask] = np.nan
     
     return res
 
