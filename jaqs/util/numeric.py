4b19ba43c45bee43860f43294ea49fcb1041aa71
manu34414 <lunli1226@gmail.com>
2018-06-28 10:29:13 +0800

    Fig bug in quantilize_without_nan()
    Fig bug in quantilize_without_nan()

--- jaqs/util/numeric.py
+++ jaqs/util/numeric.py
@@ -7,8 +7,15 @@ def quantilize_without_nan(mat, n_quantiles=5, axis=-1):
     # mask = np.isnan(mat)
     mask = pd.isnull(mat)
     res = mat.copy()
-    res[~mask] = pd.qcut(mat[~mask], n_quantiles, labels = False) + 1
 
+    nonnan_mat = mat[~mask]
+    rank = nonnan_mat.argsort(axis = axis).argsort(axis = axis)
+    count = np.sum(~mask, axis=axis)  # int
+    divisor = count * 1. / n_quantiles  # float
+    nonnan_res = np.floor(rank / divisor) + 1.0
+    res[~mask] = nonnan_res
+
+    # res[~mask] = pd.qcut(mat[~mask], n_quantiles, labels = False) + 1
     # rank = mat.argsort(axis=axis).argsort(axis=axis)  # int
     #
     # count = np.sum(~mask, axis=axis)  # int
